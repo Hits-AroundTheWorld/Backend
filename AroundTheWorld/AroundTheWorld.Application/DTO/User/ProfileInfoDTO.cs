0e55004743cacc11654c7b88496e7877b5b4cc1e
Ruslan Maksyutov <rusmaksiutov@gmail.com>
2024-06-26 23:33:12 +0700

    Работа с профилем 1) Добавил на вывод Id профиля

--- AroundTheWorld/AroundTheWorld.Application/DTO/User/ProfileInfoDTO.cs
+++ AroundTheWorld/AroundTheWorld.Application/DTO/User/ProfileInfoDTO.cs
@@ -8,6 +8,7 @@ namespace AroundTheWorld.Application.DTO.User
 {
     public class ProfileInfoDTO
     {
+        public Guid Id { get; set; }
         public string FullName { get; set; }
         public string BirthDate { get; set; }
         public string PhoneNumber { get; set; }
