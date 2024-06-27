34fe6a9149494a43c15f3e9cc897cd7e32336850
Ruslan Maksyutov <rusmaksiutov@gmail.com>
2024-06-27 14:23:10 +0700

    Работа с маппером

--- AroundTheWorld/AroundTheWorld.Application/Helpers/AutoMapper/TripMapper.cs
+++ AroundTheWorld/AroundTheWorld.Application/Helpers/AutoMapper/TripMapper.cs
@@ -2,17 +2,30 @@
 using AroundTheWorld.Domain.Entities;
 using AutoMapper;
 
-namespace AroundTheWorld.Application.Helpers.AutoMapper
+public class TripMapper : Profile
 {
-    public class TripMapper: Profile
+    public TripMapper()
     {
-        public TripMapper() { 
-            CreateMap<CreateTripInfoDTO, Trip>();
-            CreateMap<Trip, TripInfoDTO>();
-            CreateMap<ApplyForTripInfoDTO, TripAndUsers>();
-            CreateMap<User, GetTripUsersDTO>();
-            CreateMap<TripAndUsers, RequestsInfoDTO>();
-            CreateMap<TripAndUsers, GetMyRequestsDTO>();
-        }
+        CreateMap<CreateTripInfoDTO, Trip>();
+        CreateMap<Trip, TripInfoDTO>();
+        CreateMap<ApplyForTripInfoDTO, TripAndUsers>();
+        CreateMap<User, GetTripUsersDTO>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.FullName))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+            .ForMember(dest => dest.AboutMe, opt => opt.MapFrom(src => src.AboutMe))
+            .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Country))
+            .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => src.BirthDate))
+            .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber));
+        CreateMap<TripAndUsers, RequestsInfoDTO>();
+        CreateMap<TripAndUsers, GetMyRequestsDTO>();
+        CreateMap<TripAndUsers, GetTripUsersDTO>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.User.Id))
+            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.User.FullName))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email))
+            .ForMember(dest => dest.AboutMe, opt => opt.MapFrom(src => src.User.AboutMe))
+            .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.User.Country))
+            .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => src.User.BirthDate))
+            .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.User.PhoneNumber));
     }
 }
